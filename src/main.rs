03cd9a1bc1094185c44cc7925ba74bd82a08484e
andy-thomason <andy@atomicinrement.com>
2025-04-07 10:29:07 +0100

    Simplify and add comments

--- src/main.rs
+++ src/main.rs
@@ -22,21 +22,12 @@ struct Pair {
     erc20_1: Option<Erc20>,
 }
 
-macro_rules! do_call {
-    ($dest : ident, $call: ty) => {
-        let call = $call{};
-        let input = call.abi_encode();
-        let tx = TransactionRequest::default()
-            .to(address)
-            .input(input.into());
-        let Ok(res) = provider.call(tx).await else { return Ok(None) };
-    
-        let Ok(token0) = $call::abi_decode_returns(&res, false) else { return Ok(None) };
-        let $dest = token0._0;
-    }
-}
-
+/// These interfaces come from github
+/// 
+/// https://github.com/Uniswap/v2-core/tree/master/contracts
+/// 
 sol!(
+    /// All tokens support these calls and events.
     interface IERC20 {
         event Approval(address indexed owner, address indexed spender, uint value);
         event Transfer(address indexed from, address indexed to, uint value);
@@ -49,6 +40,7 @@ sol!(
         function allowance(address owner, address spender) external view returns (uint);
     }
 
+    /// All uniswap v2 pairs support these.
     interface IUniswapV2Pair {
         event Approval(address indexed owner, address indexed spender, uint value);
         event Transfer(address indexed from, address indexed to, uint value);
@@ -101,6 +93,7 @@ sol!(
     }
 );
 
+/// A "catch all" error.
 type BDE = Box<dyn std::error::Error>;
 
 #[tokio::main]
@@ -111,28 +104,23 @@ async fn main() -> Result<(), BDE> {
     let mut erc20s : HashMap<Address, Option<Erc20>> = HashMap::new();
     let mut pairs : HashMap<Address, Option<Pair>> = HashMap::new();
 
-
     // See https://docs.rs/alloy/latest/alloy/providers/trait.Provider.html
     let latest_block = provider.get_block_number().await?;
 
-    println!("latest_block={latest_block}");
-
     let swap_hash = IUniswapV2Pair::Sync::SIGNATURE_HASH;
-    let block_start = 12000000;
     let filter = Filter::new()
-        .from_block(block_start)
-        .to_block(block_start+999)
+        .from_block(latest_block-20)
+        .to_block(latest_block)
         .event_signature(swap_hash);
 
     let logs = provider.get_logs(&filter).await?;
 
-    let mut out = std::fs::File::create("/tmp/result.tsv")?;
     use std::io::Write;
-    // writeln!(out, "block_number\tcontract_address\tsender\tamount0In\tamount1In\tamount0Out\tamount1Out\tto\tsender_decimals\tsender_symbol\tto_decimals\tto_symbol")?;
+    println!("symbol_0\tsymbol_1\tprice");
 
     for l in logs {
+        let transaction_hash = l.transaction_hash.unwrap_or_default();
         let sync : alloy::rpc::types::Log<IUniswapV2Pair::Sync>  = l.log_decode().unwrap();
-        writeln!(out, "{:?}", l.transaction_hash)?;
         let block_number = sync.block_number.unwrap_or_default();
         let contract_address = sync.address();
         let IUniswapV2Pair::Sync{ reserve0, reserve1 } = sync.inner.deref();
@@ -140,14 +128,20 @@ async fn main() -> Result<(), BDE> {
         let pair = get_pair(&provider, &mut erc20s, &mut pairs, contract_address).await?;
         if let Some(pair) = pair {
             if let (Some(e0), Some(e1)) = (&pair.erc20_0, &pair.erc20_1) {
+                /// Each token has a symbol.
                 let symbol_0 = &e0.symbol;
                 let symbol_1 = &e1.symbol;
+
+                /// The decimals represent the number of digits in each price.
                 let decimals_0 = e0.decimals as i32;
                 let decimals_1 = e1.decimals as i32;
+
+                /// The price is the ratio of the reserves.
                 let r0 : f64 = reserve0.try_into().unwrap();
                 let r1 : f64 = reserve1.try_into().unwrap();
                 let price = r0 * 10_f64.powi(decimals_1-decimals_0) / r1;
-                writeln!(out, "price\t{symbol_0}/{symbol_1}\t{price}")?;
+
+                println!("{symbol_0}\t{symbol_1}\t{price}\t{transaction_hash}");
             }
         }
 
@@ -156,56 +150,57 @@ async fn main() -> Result<(), BDE> {
     Ok(())
 }
 
-
-async fn get_erc20<P : Provider>(provider: &P, erc20s: &mut HashMap<Address, Option<Erc20>>, address: Address) -> Result<Option<Erc20>, BDE> {
-    let call = IERC20::decimalsCall{};
+/// Make a call to the ethereum node via the provider.
+/// 
+/// This executes contract code to get a result.
+async fn do_call<P : Provider, Call : SolCall>(provider: &P, call: Call, address: Address) -> Result<Option<Call::Return>, BDE> {
     let input = call.abi_encode();
     let tx = TransactionRequest::default()
         .to(address)
         .input(input.into());
     let Ok(res) = provider.call(tx).await else { return Ok(None) };
-
-    let Ok(decimals) = IERC20::decimalsCall::abi_decode_returns(&res, false) else { return Ok(None) };
-    let decimals = decimals._0;
-
-    let call = IERC20::symbolCall{};
-    let input = call.abi_encode();
-    let tx = TransactionRequest::default()
-        .to(address)
-        .input(input.into());
-    let Ok(res) = provider.call(tx).await else { return Ok(None) };
-
-    let Ok(symbol) = IERC20::symbolCall::abi_decode_returns(&res, false) else { return Ok(None) };
-    let symbol = symbol._0.into();
-
-    Ok(Some(Erc20 {
-        decimals,
-        symbol,
-    }))
+    
+    Ok(Some(Call::abi_decode_returns(&res, false)?))
 }
 
+
+/// Fetch ERC20 token data.
+/// 
+/// This executes contract code to get a result.
+async fn get_erc20<P : Provider>(provider: &P, erc20s: &mut HashMap<Address, Option<Erc20>>, address: Address) -> Result<Option<Erc20>, BDE> {
+    if let Some(erc20) = erc20s.get(&address) {
+        // Seen this one before.
+        return Ok(erc20.clone());
+    }
+
+    let Some(decimals) = do_call(provider, IERC20::decimalsCall{}, address).await? else { return Ok(None) };
+    let decimals = decimals._0;
+
+    let Some(symbol) = do_call(provider, IERC20::symbolCall{}, address).await? else { return Ok(None) };
+    let symbol = symbol._0.into();
+
+    let erc20 = Some(Erc20 {
+        decimals,
+        symbol,
+    });
+
+    erc20s.insert(address, erc20.clone());
+
+    Ok(erc20)
+}
+
+/// Fetch pair information for a swap.
+/// 
 async fn get_pair<P : Provider>(provider: &P, erc20s: &mut HashMap<Address, Option<Erc20>>, pairs : &mut HashMap<Address, Option<Pair>>, address: Address) -> Result<Option<Pair>, BDE> {
     if let Some(pair) = pairs.get(&address) {
+        // Seen this one before.
         return Ok(pair.clone());
     }
-    let call = IUniswapV2Pair::token0Call{};
-    let input = call.abi_encode();
-    let tx = TransactionRequest::default()
-        .to(address)
-        .input(input.into());
-    let Ok(res) = provider.call(tx).await else { return Ok(None) };
 
-    let Ok(token0) = IUniswapV2Pair::token0Call::abi_decode_returns(&res, false) else { return Ok(None) };
+    let Some(token0) = do_call(provider, IUniswapV2Pair::token0Call{}, address).await? else { return Ok(None) };
     let token0 = token0._0;
 
-    let call = IUniswapV2Pair::token1Call{};
-    let input = call.abi_encode();
-    let tx = TransactionRequest::default()
-        .to(address)
-        .input(input.into());
-    let Ok(res) = provider.call(tx).await else { return Ok(None) };
-
-    let Ok(token1) = IUniswapV2Pair::token1Call::abi_decode_returns(&res, false) else { return Ok(None) };
+    let Some(token1) = do_call(provider, IUniswapV2Pair::token1Call{}, address).await? else { return Ok(None) };
     let token1 = token1._0;
 
     let erc20_0 = get_erc20(provider, erc20s, token0).await?;
